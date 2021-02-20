e31f0b87e0867c8bbd5c981d4a06e5e78d700655
Karol Moroz <k.j.moroz@gmail.com>
2021-02-20 18:40:13 +0800

    Generate Elixir app

---
+++ consumer/lib/consumer.ex
@@ -0,0 +1,18 @@
+defmodule Consumer do
+  @moduledoc """
+  Documentation for `Consumer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Consumer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
